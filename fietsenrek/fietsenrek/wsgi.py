a74ed14609d0111780b5ac9adb85647da82883d9
Marta Rożek <marta.rozek@gmail.com>
2016-10-28 18:25:41 +0200

    Set local settings as default in manage.py and wsgi.py.

--- fietsenrek/fietsenrek/wsgi.py
+++ fietsenrek/fietsenrek/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fietsenrek.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fietsenrek.settings.local")
 
 application = get_wsgi_application()
