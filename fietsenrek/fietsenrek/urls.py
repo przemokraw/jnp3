bba174e808e9ef41da95ba619967a1ef854f239d
Marta Rożek <marta.rozek@gmail.com>
2016-12-27 15:12:06 +0100

    Change authentication to rest.
    About API: http://django-rest-auth.readthedocs.io/en/latest/api_endpoints.html

--- fietsenrek/fietsenrek/urls.py
+++ fietsenrek/fietsenrek/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from . import views
+from accounts.views import FacebookLogin
 
 urlpatterns = [
-    url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^admin/', admin.site.urls,),
-    url(r'^accounts/', include('allauth.urls', namespace='accounts')),
+    url(r'^rest-auth/', include('rest_auth.urls')),
+    url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
     url(r'^racks/', include('racks.urls', namespace='racks')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
