e9a4e4c235b117be038e18c9934549b2ab21bcfe
Marta Rożek <marta.rozek@gmail.com>
2017-01-24 12:57:39 +0100

    Add memcached and postgres to Docker. Lacks UWSGI.

--- fietsenrek/fietsenrek/settings/production.py
+++ fietsenrek/fietsenrek/settings/production.py
@@ -15,11 +15,10 @@ INSTALLED_APPS += [
 DATABASES = {
     'default': {
         'ENGINE': 'django_postgrespool',
-        'NAME': 'fietsenrek',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
@@ -27,4 +26,17 @@ DATABASE_POOL_ARGS = {
     'max_overflow': 10,
     'pool_size': 5,
     'recycle': 300,
+}
+
+
+# Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': [
+            'cache1:11211',
+            'cache2:11212',
+        ]
+    }
 }
